39e06f699443caa355e59d018d80b90cffe05a45
Roman Xaver Poledniok <piarkeys123@gmail.com>
2022-10-28 00:58:23 +0200

    update game file

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
 '''
-Welcome to the game "Kostki"!
-The way you play the game is pretty simple:
-
-In the beginning you roll your dice and try to collect points.
-
-The points are counted as following:
-Only 1 and 5 can be picked up separately. 1 = 100 points and 5 = 50 points
-3x 2 = 200 points (3x 3 = 300 and so forth) except for 3x 1 which is 1000
-6x 2 = 2000 points (3x 3 = 300 and so forth) except for 6x 1 which is 10000
-
-Phase 1:
-In the first phase every player first needs to reach at least 1050 points or more, in order to reach the phase two
-
-Phase 2:
-In the second phase of the game every player can write down any points number if its at least 350 points.
-
-Phase 3:
-As soon as the first player hits 10050 points, every player coming after him still gets one try. So if your turn was
-before the players who hit 10050, that is bad luck. This is also the reason why you always want to be the last person to
-throw your dice. In the end the player wins who has the most points.
+All relevant information can be found in the README.md
 '''
 
 def initialize_player():
